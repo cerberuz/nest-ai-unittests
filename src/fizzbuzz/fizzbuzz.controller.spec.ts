112acc866dad073a66f1a88a5228a1ea1bf88fd6
Jesus <jesus@Jesuss-MacBook-Pro-2.local>
2025-12-02 11:31:16 -0300

    addin unit tests for fizzbuzz modlues

--- src/fizzbuzz/fizzbuzz.controller.spec.ts
+++ src/fizzbuzz/fizzbuzz.controller.spec.ts
@@ -1,18 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FizzbuzzController } from './fizzbuzz.controller';
+import { FizzbuzzService } from './fizzbuzz.service';
 
 describe('FizzbuzzController', () => {
   let controller: FizzbuzzController;
+  let service: FizzbuzzService;
+  let spy: jest.SpyInstance;
+  const mockNumber = 15;
+  const mockResult = 'FizzBuzz';
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FizzbuzzController],
+      providers: [FizzbuzzService],
     }).compile();
 
     controller = module.get<FizzbuzzController>(FizzbuzzController);
+    service = module.get<FizzbuzzService>(FizzbuzzService);
+    spy = jest.spyOn(service, 'fizzbuzz').mockReturnValue(mockResult);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it('should return the correct Fizz buzz word for the given number (using spyOn)', () => {
+    const result = controller.fizzbuzz(mockNumber.toString());
+    expect(spy).toHaveBeenCalledWith(mockNumber);
+    expect(result).toBe(mockResult);
   });
 });
